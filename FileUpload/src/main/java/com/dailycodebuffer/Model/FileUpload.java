116d135fa2f2be8120e35faa4157948685df6c0d
Praveena <Praveena@DESKTOP-O5D0MDQ>
2021-02-05 10:13:25 +0530

    New Entity FileUpload Added

---
+++ FileUpload/src/main/java/com/dailycodebuffer/Model/FileUpload.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.dailycodebuffer.Model;
+
+/**
+ * @author Praveena
+ *
+ */
+public class FileUpload {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	
+
+}
